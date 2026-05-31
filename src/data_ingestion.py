143cfce3055b24390c85eb56b64828005d065086
databybishal <bishalkoirala89@gmail.com>
2026-05-31 14:54:08 +0545

    done

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import logging
+import pandas as pd
 from apiSource.data_source import source
 
 
@@ -9,13 +10,30 @@ from utils import extractApis
 
 
 log = logging.getLogger('ingestion')
-
 api = source['stock_price_apis']['api']
 
 if __name__ == '__main__':
+    try:
+        log.info("Preparing for data ingestion")
+        data = extractApis(api)
+        time_series = data['Time Series (Daily)']
+        df = pd.DataFrame(
+            [
+                {
+                    'date': date,
+                    'open': value['1. open'],
+                    'high': value['2. high'],
+                    'low': value['3. low'],
+                    'close': value['4. close'],
+                    'volume': value['5. volume']
+                }
+                for date, value in time_series.items()
+            ]
+        )
+        print(df.head())
+    except Exception as e:
+        pass
     
-    
-
 
 
 
